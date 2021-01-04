9581e247bbc3a6115ce3ca46724eea34276eb9c8
vincent <vincent@192.168.1.16>
2021-01-04 15:13:06 +0100

    city.distanceTo() - new tests added

--- src/test/java/tsp/lee/jacobson/CityTest.java
+++ src/test/java/tsp/lee/jacobson/CityTest.java
@@ -13,12 +13,12 @@ public class CityTest {
 	 * THEN 1.4142
 	 */
 	public void testDistanceTo_00_to_11() {
-			City city1 = new City(0,0);
-			City cityToTest = new City(1,1);
-			double delta = 0.0001;
-			double expected = 1.4142;
-			double actual = city1.distanceTo(cityToTest);
-			assertEquals(expected, actual, delta);
+		City city1 = new City(0,0);
+		City cityToTest = new City(1,1);
+		double delta = 0.0001;
+		double expected = 1.4142;
+		double actual = city1.distanceTo(cityToTest);
+		assertEquals(expected, actual, delta);
 	}
 	
 	@Test
@@ -28,10 +28,55 @@ public class CityTest {
 	 * THEN 0
 	 */
 	public void testDistanceTo_self() {
-			City city1 = new City(3,5);
-			double delta = 0.0001;
-			double expected = 0;
-			double actual = city1.distanceTo(city1);
-			assertEquals(expected, actual, delta);
+		City city1 = new City(3,5);
+		double delta = 0.0001;
+		double expected = 0;
+		double actual = city1.distanceTo(city1);
+		assertEquals(expected, actual, delta);
+	}
+
+	@Test
+	/**
+	 * GIVEN distanceTo
+	 * WHEN position of city1 = 1;2 and position of cityToTest = 4;3
+	 * THEN 3.1623
+	 */
+	public void testDistanceTo_12_to_43() {
+		City city1 = new City(1,2);
+		City cityToTest = new City(4,3);
+		double delta = 0.0001;
+		double expected = 3.1623;
+		double actual = city1.distanceTo(cityToTest);
+		assertEquals(expected, actual, delta);
+	}
+
+	@Test
+	/**
+	 * GIVEN distanceTo
+	 * WHEN position of city1 = 0;0 and position of cityToTest = -4;3
+	 * THEN 5
+	 */
+	public void testDistanceTo_00_to_minus43() {
+		City city1 = new City(0,0);
+		City cityToTest = new City(-4,3);
+		double delta = 0.0001;
+		double expected = 5;
+		double actual = city1.distanceTo(cityToTest);
+		assertEquals(expected, actual, delta);
+	}
+
+	@Test
+	/**
+	 * GIVEN distanceTo
+	 * WHEN position of city1 = 1;2 and position of cityToTest = -4;-3
+	 * THEN 7.0711
+	 */
+	public void testDistanceTo_12_to_minus4minus3() {
+		City city1 = new City(1,2);
+		City cityToTest = new City(-4,-3);
+		double delta = 0.0001;
+		double expected = 7.0711;
+		double actual = city1.distanceTo(cityToTest);
+		assertEquals(expected, actual, delta);
 	}
 }
