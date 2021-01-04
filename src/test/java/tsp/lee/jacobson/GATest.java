75aa4b14b199c22b2d8cbfb30dd3167b597fc284
vincent <vincent@192.168.1.16>
2021-01-04 15:11:40 +0100

    GA.evolvePopulation(), elitism tested

--- src/test/java/tsp/lee/jacobson/GATest.java
+++ src/test/java/tsp/lee/jacobson/GATest.java
@@ -7,8 +7,20 @@ import org.junit.Test;
 public class GATest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	/**
+	 * GIVEN evolvePopulation
+	 * WHEN elitism = true
+	 * THEN tour at index 0 of newPopulation = fittest of oldPopulation
+	 */
+	public void testEvolvePopulation() {
+		Population population1 = new Population(5, true);
+		Population population2 = new Population(5, false);
+		
+		population2 = GA.evolvePopulation(population1);
+		
+		Tour expected = population1.getFittest();
+		Tour actual = population2.tours[0];
+		assertEquals(expected, actual);
 	}
 
 }
